af3f14e237492210ddbb7d8a1938172f66903415
Afzal Ashraf <2020cs31@student.uet.edu.pk>
2022-07-17 19:44:32 +0500

    Navbar completed and added breakpoint setttings

---
+++ src/Styles.js
@@ -0,0 +1,8 @@
+import "./sass/base/_reset.scss"
+import "./sass/base/_typography.scss"
+
+import "./sass/layout/_navigation.scss"
+
+import "./sass/components/_buttons.scss"
+
+import "./sass/layout/_breakpoints.scss"
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Logo from "../images/Logo.svg"
+import Nav from "../images/nav.svg"
+import Button from './Button'
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+                <img src={Logo} alt="" />
+            </div>
+            <div className="nav-links">
+                <a href="#/">Marketplace</a>
+                <a href="#/">Artists</a>
+                <a href="#/">Commmunity</a>
+            </div>
+            <span className="ham-burger">
+                <img src={Nav} alt="" />
+
+            </span>
+            <div className="register">
+                <Button text={"Register"} />
+            </div>
+        </nav>
+    )
+}
